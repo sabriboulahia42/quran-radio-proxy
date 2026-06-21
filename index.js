c7cb8748bf6c12061c7441c3bd669183fa06b631
sabriboulahia42 <sabriboulahia42@gmail.com>
2026-06-21 10:32:34 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-    // Dynamically captures the current domain and appends the api route and incoming queries
-    const queryString = req.url.includes('?') ? req.url.substring(req.url.indexOf('?')) : '';
-    
-    res.writeHead(302, {
-        'Location': `/api${queryString}`
-    });
-    res.end();
-};
